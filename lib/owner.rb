3934e02e972cd3eeedc77a9ea083b46251449e64
Juan Rivera <juanriv45@gmail.com>
2018-02-28 05:20:51 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,6 +30,11 @@ class Owner
       cat.mood = "happy"
     end
   end
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
   def self.all
     @@all
   end
