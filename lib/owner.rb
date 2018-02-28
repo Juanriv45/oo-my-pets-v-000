48ae0ce9f9806bff8ad119063acc0477b45d2ccd
Juan Rivera <juanriv45@gmail.com>
2018-02-28 05:28:24 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -44,7 +44,7 @@ class Owner
     @pets.clear
   end
   def list_pets
-    "I have #{@pets[:fishes].count}"
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end
   def self.all
     @@all
